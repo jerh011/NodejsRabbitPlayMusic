58528ff79521c4d27a6351f0264d5af5e90c0ed3
Jerh011 <jeelrohe@gmail.com>
2025-10-29 16:19:31 -0700

    practicapwaApi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TaskModule } from './prisma/Task/Task.module';
+
 import { CancionesModule } from './prisma/Canciones/Canciones.module';
 import { ArtistaModule } from './prisma/Artistas/Artistas.module';
 import { AlbumesModule } from './prisma/Albumes/Albumes.module';
@@ -7,7 +7,6 @@ import { GenerosModule } from './prisma/Generos/Generos.module';
 import { FavoritosModule } from './prisma/Favoritos/Favoritos.module';
 @Module({
   imports: [
-    TaskModule,
     CancionesModule,
     ArtistaModule,
     AlbumesModule,
