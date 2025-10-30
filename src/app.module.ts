7c2ab57f5a007e29b7e1a02a165c23d81a166ecd
Jerh011 <jeelrohe@gmail.com>
2025-10-29 18:49:45 -0700

    cambios al despligue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,7 @@ import { CancionesModule } from './prisma/Canciones/Canciones.module';
 import { ArtistaModule } from './prisma/Artistas/Artistas.module';
 import { AlbumesModule } from './prisma/Albumes/Albumes.module';
 import { GenerosModule } from './prisma/Generos/Generos.module';
-import { FavoritosModule } from './prisma/Favoritos/Favoritos.module';
 @Module({
-  imports: [
-    CancionesModule,
-    ArtistaModule,
-    AlbumesModule,
-    GenerosModule,
-    FavoritosModule,
-  ],
+  imports: [CancionesModule, ArtistaModule, AlbumesModule, GenerosModule],
 })
 export class AppModule {}
